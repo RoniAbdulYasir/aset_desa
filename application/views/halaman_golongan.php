afe409d7a0a59215ce602f2db746f417c39b7591
unang <acedefire@gmail.com>
2020-10-10 08:32:18 +0700

    datatable load
    dont use datatable in shared  template

--- application/views/halaman_golongan.php
+++ application/views/halaman_golongan.php
@@ -50,4 +50,23 @@
 	</div>
 
 </section>
+
+
+ <script>
+  $(document).ready(function () {
+    $("#example1").DataTable({
+      "responsive": true,
+      "autoWidth": false,
+    });
+    $('#example2').DataTable({
+      "paging": true,
+      "lengthChange": false,
+      "searching": false,
+      "ordering": true,
+      "info": true,
+      "autoWidth": false,
+      "responsive": true,
+    });
+  });
+</script>
 <!-- /.content -->
